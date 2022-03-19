1194ad7f1648c139670cafe1d33acac3fd1eb901
UnitedIngvar <ingvarpetrova@gmail.com>
2022-03-19 14:17:15 +0300

    coded Contact class header

---
+++ MODULE_00/ex_01/includes/Contact.hpp
@@ -0,0 +1,29 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+class Contact
+{
+private:
+	int			id;
+	std::string	first_name;
+	std::string	last_name;
+	std::string	nickname;
+	std::string	phone_number;
+	std::string	darkest_secret;
+
+public:
+	Contact(int id,
+	std::string first_name,
+	std::string last_name,
+	std::string nickname,
+	std::string phone_number,
+	std::string darkest_secret);
+
+	std::string GetFirstName();
+	std::string GetLastName();
+	std::string GetNickname();
+	std::string GetPhoneNumber();
+	std::string GetDarkestSecret();
+};
+
+#endif
